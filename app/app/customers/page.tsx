4b65ce06ecca152603d4a827408438d60480524b
rudrava <rudravamukherjee.2001@gmail.com>
2024-05-20 02:48:56 +0530

    fix: text color

--- app/app/customers/page.tsx
+++ app/app/customers/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Customers = () => {
   return (
     <div className="grid place-items-center">
-      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20 ">
+      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center  relative z-20 ">
         Customers
       </h1>
     </div>
--- app/app/home/page.tsx
+++ app/app/home/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="grid place-items-center">
-      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20 ">
+      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center  relative z-20 ">
         Home
       </h1>
     </div>
